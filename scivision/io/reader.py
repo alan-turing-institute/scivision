13bf282b80aaa927482cbaffc282fd72d509f704
quantumjot <code@arlowe.co.uk>
2021-07-29 13:41:41 +0100

    allow urls and local config files

--- scivision/io/reader.py
+++ scivision/io/reader.py
@@ -1,5 +1,5 @@
 import os
-from urllib.parse import urljoin
+from urllib.parse import urlparse
 
 import fsspec
 import yaml
@@ -10,8 +10,34 @@ from .wrapper import PretrainedModel
 SCIVISION_YAML_CONFIG = ".scivision-config.yaml"
 
 
-def _parse_config(path: os.PathLike) -> dict:
+def _is_url(path: os.PathLike) -> bool:
+    return urlparse(path).scheme in ("http", "https",)
+
+
+def _parse_url(path: os.PathLike, branch: str = "main"):
+    """Parse a URL and convert to a raw github url if necessary."""
+    parsed = urlparse(path)
+    if not parsed.netloc == "github.com":
+        return parsed.geturl()
+
+    # construct the new github path
+    parsed = parsed._replace(netloc="raw.githubusercontent.com")
+    split = list(filter(None, parsed.path.split("/")))
+    new_path = "/".join(split[:2]) + f"/{branch}/" + "/".join(split[2:])
+    parsed = parsed._replace(path=new_path)
+    return parsed.geturl()
+
+
+def _parse_config(path: os.PathLike, branch: str = "main") -> dict:
     """Parse the `scivision` config file."""
+
+    # check that this is a path to a yaml file
+    if not path.endswith((".yml", ".yaml",)):
+        raise ValueError(f"Invalid configuration filename: {path}")
+
+    if _is_url(path):
+        path = _parse_url(path)
+
     file = fsspec.open(path)
     with file as config_file:
         stream = config_file.read()
@@ -25,11 +51,10 @@ def _parse_config(path: os.PathLike) -> dict:
 
 def load_pretrained_model(
     path: os.PathLike,
-    branch: str = 'main',
-    config_filename: str = SCIVISION_YAML_CONFIG,
+    branch: str = "main",
     allow_install: bool = False,
     *args,
-    **kwargs
+    **kwargs,
 ) -> PretrainedModel:
     """Load a pre-trained model.
 
@@ -37,6 +62,8 @@ def load_pretrained_model(
     ----------
     path : PathLike
         The filename, path or URL of a pretrained model description.
+    branch : str, default = main
+        Specify the name of a github branch if loading from github.
     allow_install : bool
         Allow installation of remote package via pip.
 
@@ -46,15 +73,8 @@ def load_pretrained_model(
         The instantiated pre-trained model.
     """
 
-    # we first need to parse the yaml file if it exists
-    # NOTE(arl): this assumes we're grabbing a model from github, but it should
-    # also work with a local config file with a reference to a github repo
-    config_url = urljoin(
-        f"https://raw.githubusercontent.com/{path}/{branch}/",
-        config_filename,
-    )
-
-    config = _parse_config(config_url)
+    # parse the config file
+    config = _parse_config(path, branch=branch)
 
     # try to install the package if necessary
     install_package(config, allow_install=allow_install)
