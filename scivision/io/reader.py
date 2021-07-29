2861086f01356ac1716964a20bebba249c0239b1
Alejandro © <acocac@gmail.com>
2021-07-29 09:18:04 +0100

    Update scivision/io/reader.py
    Co-authored-by: Alan R Lowe <code@arlowe.co.uk>

--- scivision/io/reader.py
+++ scivision/io/reader.py
@@ -24,7 +24,12 @@ def _parse_config(path: os.PathLike) -> dict:
 
 
 def load_pretrained_model(
-    path: os.PathLike, branch: str = 'main', allow_install: bool = False, *args, **kwargs
+    path: os.PathLike, 
+    branch: str = 'main', 
+    config_filename: str = SCIVISION_YAML_CONFIG,
+    allow_install: bool = False, 
+    *args, 
+    **kwargs
 ) -> PretrainedModel:
     """Load a pre-trained model.
 
