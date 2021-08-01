08eb6585688ab70760accd4f14774471bb1cf69d
quantumjot <code@arlowe.co.uk>
2021-08-01 15:38:59 +0100

    add exception handling and some docstrings

--- scivision/koala.py
+++ scivision/koala.py
@@ -2,6 +2,9 @@ import inspect
 import io
 import logging
 from functools import wraps
+from typing import Callable
+
+KOALA_LOGGING_LEVEL = logging.DEBUG
 
 # get the logger instance
 logger = logging.getLogger(__name__)
@@ -10,33 +13,56 @@ stream = io.StringIO()
 
 # if we don't have any handlers, set one up
 if not logger.handlers:
-    # configure stream handler
-    log_fmt = logging.Formatter(
+    format = logging.Formatter(
         "\U0001f428 [%(levelname)s][%(asctime)s] %(message)s",
-        datefmt="%Y/%m/%d %I:%M:%S %p",
+        datefmt="%Y/%m/%d %H:%M:%S",
     )
-    console_handler = logging.StreamHandler(stream)
-    console_handler.setFormatter(log_fmt)
+    handler = logging.StreamHandler(stream)
+    handler.setFormatter(format)
+    logger.addHandler(handler)
+    logger.setLevel(KOALA_LOGGING_LEVEL)
 
-    logger.addHandler(console_handler)
-    logger.setLevel(logging.DEBUG)
 
+def koala(fn: Callable) -> Callable:
+    """Decorator to enable logging of calls to core `scivision` functions.
 
-def koala(fn):
-    """Log calls to core `scivision` functions"""
+    Parameters
+    ----------
+    fn : Callable
+        The function to be wrapped with the `koala` logger.
 
-    @wraps(koala)
+    Returns
+    -------
+    wrapped_fn : Callable
+        The `koala` wrapped function.
+    """
+
+    @wraps(fn)
     def wrapped_fn(*args, **kwargs):
-        output = fn(*args, **kwargs)
+
         # TODO(arl): store the arguments and output of the function
         logger.info(
             f"Call='{fn.__name__}', module='{inspect.getmodule(fn).__name__}'"
+            # f", kwargs='{', '.join(kwargs.keys())}'"
         )
+
+        # now call the function, but log any exceptions
+        try:
+            output = fn(*args, **kwargs)
+        except Exception as e:
+            logger.error(e)
+            raise
         return output
 
     return wrapped_fn
 
 
-def get_koala_log():
-    """Get the koala log."""
+def get_koala_log() -> str:
+    """Get the koala log.
+
+    Returns
+    -------
+    stream : str
+        The koala log stream as a string.
+    """
     return stream.getvalue()
