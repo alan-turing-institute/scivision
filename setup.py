fdd3232ff311f23d46ff8e9b3e6e5dbff9bfc841
Alejandro Coca <acocac@gmail.com>
2021-08-06 11:27:24 +0100

    creating setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+requirements = []
+with open("requirements.txt") as f:
+    for line in f:
+        stripped = line.split("#")[0].strip()
+        if len(stripped) > 0:
+            requirements.append(stripped)
+
+setup(
+    name="scivision",
+    version="0.0.1",
+    description="scivision",
+    author="Scivision Community",
+    author_email="alowe@turing.ac.uk",
+    url="https://github.com/alan-turing-institute/scivision",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.7",
+)
