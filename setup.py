7ecf08139b308a9d673ab4929e3345a2d4049a21
ots22 <ots22@users.noreply.github.com>
2021-11-24 17:11:56 +0000

    Update setup.py with project email address

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     version="0.0.1",
     description="scivision",
     author="Scivision Community",
-    author_email="scivision@unknown.invalid",
+    author_email="scivision@turing.ac.uk",
     url="https://github.com/alan-turing-institute/scivision",
     packages=find_packages(),
     install_requires=requirements,
