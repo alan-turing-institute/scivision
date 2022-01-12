1ca5533321a812979780afcfae8953e75be1ad8b
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-01-12 16:32:50 +0000

    flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,15 +11,18 @@ with file as config_file:
     stream = config_file.read()
     imagenet_model_config = yaml.safe_load(stream)
 
+
 # Create the model
 @pytest.fixture(scope='session', autouse=True)
 def IMAGENET_MODEL(request):
     return PretrainedModel(imagenet_model_config)
 
+
 # The dataset to run model on
 @pytest.fixture(scope='session', autouse=True)
 def KOALA(request):
     return 'tests/koala.jpeg'
+
 
 # Install the model package so it can be used in tests
 install_package(imagenet_model_config, allow_install=True)
