4ac92a2ef44cb73adc0aa29d15f821175bb94779
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-01-12 13:48:30 +0000

    add tests for pretrained model

---
+++ tests/test_wrapper.py
@@ -0,0 +1,26 @@
+from scivision.io.wrapper import PretrainedModel
+from skimage.io import imread
+from skimage.transform import resize
+from tensorflow.keras.applications.imagenet_utils import decode_predictions
+
+import fsspec
+import yaml
+
+file = fsspec.open('tests/test_model_scivision.yml')
+with file as config_file:
+    stream = config_file.read()
+    IMAGENET_MODEL_CONFIG = yaml.safe_load(stream)
+IMAGENET_MODEL = PretrainedModel(IMAGENET_MODEL_CONFIG)
+KOALA = 'tests/koala.jpeg'
+
+def test_pretrained_model():
+    """Check that a pretrained model object can be created from config"""
+    assert type(IMAGENET_MODEL) == PretrainedModel
+
+
+def test_pretrained_model_predict():
+    """Check that a pretrained model's predict function works as expected"""
+    image = resize(imread(KOALA), (224, 224), preserve_range=True, anti_aliasing=True)
+    y = IMAGENET_MODEL.predict(image)
+    _, image_class, class_confidence = decode_predictions(y, top=1)[0][0]
+    assert image_class == 'koala'
