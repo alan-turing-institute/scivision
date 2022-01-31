a0a94db9a00d840c34e14e0a296c1d67b84f7340
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-01-31 17:01:14 +0000

    add launch_pull_request_function (untested)

--- scivision/catalog/update.py
+++ scivision/catalog/update.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+from github import Github
 import json
 
 
@@ -24,6 +25,41 @@ def _update_catalog(entry: str, catalog: str) -> None:
     catalog_dict = catalog_dict | entry_dict  # Note: Python 3.9+ only
     with open(catalog, 'w') as old_catalog:
         json.dump(catalog_dict, old_catalog, sort_keys=True, indent=4)
+        
+        
+def _launch_pull_request(catalog):
+    """Sends a pull request to the scivision-catalog repo, adding to the scivision catalog files
+    """
+    if 'datasources.json' in catalog:
+        catalog_name = 'datasources'
+    elif 'models.json' in catalog:
+        catalog_name = 'models'
+    catalog_file = catalog_name + ".json"
+    
+    # Configure PyGitHub
+    print('Paste your GitHub access token:')
+    token = input()
+    g = Github(token)
+    repo = g.get_repo("alan-turing-institute/scivision-catalog")
+    
+    # Create new branch in the scivision-catalog repo
+    print('In four words or fewer, describe your addition to the scivision catalog:')
+    desc = input()
+    target_branch = desc.replace(' ', '-')
+    main_branch = repo.get_branch('main')
+    repo.create_git_ref(ref='refs/heads/' + target_branch, sha=main_branch.commit.sha)
+    
+    # Create a commit
+    contents = repo.get_contents(catalog_file, ref=target_branch)
+    with open(catalog, 'r') as file:
+        catalog_string = file.read()
+    repo.update_file(contents.path, desc, catalog_string, contents.sha, branch=target_branch)
+
+    # Create a PR
+    body = '# Add an entry to the scivision ' + catalog_name + ' catalog'
+    body += '\n'
+    body += desc
+    repo.create_pull(title=desc, body=body, head=target_branch, base="main")
 
 
 def add_dataset(dataset: str, catalog: str) -> None:
