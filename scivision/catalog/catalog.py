f796f2a72a534fff67437464f92a378b8f77fb93
Oliver Strickson <ostrickson@turing.ac.uk>
2022-02-07 10:33:02 +0000

    Introduce pydantic classes for model/data catalogues and corresponding adjustments to data

--- scivision/catalog/catalog.py
+++ scivision/catalog/catalog.py
@@ -1,27 +1,78 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 import pkgutil
-from typing import Dict, List
-
 import pandas as pd
+import os
+from pathlib import Path
+from typing import Dict, List, Optional
+from pydantic import BaseModel
+from enum import Enum
 
 from .base_catalog import BaseCatalog
 from ..koala import koala
+
+
+class TaskEnum(str, Enum):
+    classificiation = "classification"
+    object_detection = "object-detection"
+    segmentation = "segmentation"
+    thresholding = "thresholding"
+
+
+class ModelCatalogEntry(BaseModel, extra="forbid"):
+    name: str
+    description: Optional[str]
+    tasks: List[TaskEnum]
+    url: str
+    pkg_url: str
+    format: str
+    pretrained: bool
+    labels_required: bool
+    language: Optional[str]
+    institution: Optional[str]
+    tags: List[str]
+
+
+class ModelCatalog(BaseModel, extra="forbid"):
+    catalog_type: str = "scivision model catalog"
+    name: str
+    entries: List[ModelCatalogEntry]
+
+
+class DataCatalogEntry(BaseModel, extra="forbid"):
+    name: str
+    description: Optional[str]
+    tasks: List[TaskEnum]
+    domains: List[str]
+    url: str
+    format: str
+    labels: bool
+    institution: Optional[str]
+    tags: List[str]
+
+
+class DataCatalog(BaseModel, extra="forbid"):
+    catalog_type: str = "scivision datasource catalog"
+    name: str
+    entries: List[DataCatalogEntry]
 
 
 class PandasCatalog(BaseCatalog):
     def __init__(self):
         super().__init__()
 
-        datasources_uri = pkgutil.get_data(__name__, "data/datasources.json")
-        models_uri = pkgutil.get_data(__name__, "data/models.json")
+        datasources_raw = pkgutil.get_data(__name__, "data/datasources.json")
+        models_raw = pkgutil.get_data(__name__, "data/models.json")
 
-        datasources = pd.read_json(datasources_uri, orient="index")
-        models = pd.read_json(models_uri, orient="index")
+        datasources_cat = DataCatalog.parse_raw(datasources_raw)
+        models_cat = ModelCatalog.parse_raw(models_raw)
 
-        self._models = models.explode("task").explode("data_format")
-        self._datasources = datasources.explode("task").explode("format")
+        datasources = pd.DataFrame([ent.dict() for ent in datasources_cat.entries])
+        models = pd.DataFrame([ent.dict() for ent in models_cat.entries])
+
+        self._models = models.explode("tasks").explode("format")
+        self._datasources = datasources.explode("tasks").explode("format")
 
     @property
     def _database(self) -> pd.DataFrame:
@@ -29,8 +80,8 @@ class PandasCatalog(BaseCatalog):
             self._datasources,
             suffixes=("_model", "_data"),
             how="inner",
-            left_on=["task", "data_format"],
-            right_on=["task", "format"],
+            left_on=["tasks", "format"],
+            right_on=["tasks", "format"],
         )
 
     def _query(self, query: Dict[str, str]) -> list:
