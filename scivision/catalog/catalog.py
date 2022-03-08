4ece1984c563fab55c3e891b93855a766683b895
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-03-08 17:00:37 +0000

    allow multiple institutions datasets

--- scivision/catalog/catalog.py
+++ scivision/catalog/catalog.py
@@ -49,7 +49,7 @@ class CatalogDatasourceEntry(BaseModel, extra="forbid"):
     url: Union[AnyUrl, FileUrl]
     format: str
     labels_provided: bool
-    institution: Optional[str]
+    institution: Optional[Tuple[str, ...]]
     tags: Tuple[str, ...]
 
     def __getitem__(self, item):
