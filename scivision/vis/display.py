ab3155dcacb122d1cb5d49ee2ca484c3207bf095
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-09 13:29:27 +0100

    use distinctipy for unique colour bounding boxes

--- scivision/vis/display.py
+++ scivision/vis/display.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+from distinctipy import distinctipy
 from matplotlib.image import AxesImage
 from matplotlib.colors import rgb2hex
 import numpy as np
@@ -9,12 +10,11 @@ from PIL import Image, ImageDraw, ImageFont
 import os.path
 
 
-def _draw_bounding_box(im, score, xmin, ymin, xmax, ymax, index, num_boxes, font):
+def _draw_bounding_box(im, score, xmin, ymin, xmax, ymax, index, num_boxes, font, color_rgb):
     """Draw a bounding boxes for object detection."""
     im_with_rectangle = ImageDraw.Draw(im)  
-    color = rgb2hex((0.0, 1.0, 0.0))
-    im_with_rectangle.rounded_rectangle((xmin, ymin, xmax, ymax), outline = color, width = 2, radius = 10)
-    # im_with_rectangle.text((xmax-xmin, ymax-ymin), str(index), fill="blue", stroke_fill = "red", font=font)
+    color_hex = rgb2hex(color_rgb)
+    im_with_rectangle.rounded_rectangle((xmin, ymin, xmax, ymax), outline = color_hex, width = 2, radius = 10)
     return im
 
 
@@ -25,20 +25,29 @@ def predplot(image: np.ndarray,
     from a scivision model"""
     if task != "object detection":
         raise NotImplementedError("Visualisation for model predictions other than object detection have not been developed yet")
-    
+
     pillow_image = Image.fromarray(image.to_numpy(), 'RGB')
+
     font_path = os.path.abspath(os.path.dirname(__file__)) + '/fonts/arial.ttf'
     font = ImageFont.truetype(font_path, 20)
+
     num_boxes = len(predictions)
+    # generate visually distinct colours for each bounding box
+    colors = distinctipy.get_colors(num_boxes)
+
     index = 1
     for bounding_box in predictions:
         box = bounding_box["box"]
-        bounded_image = _draw_bounding_box(pillow_image, bounding_box["score"],\
-            box["xmin"], box["ymin"], box["xmax"], box["ymax"], index, num_boxes, font)
+        bounded_image = _draw_bounding_box(pillow_image,
+                                           bounding_box["score"],
+                                           box["xmin"], box["ymin"],
+                                           box["xmax"], box["ymax"],
+                                           index, num_boxes, font,
+                                           colors[index-1])
         index += 1
 
     display(bounded_image)
-    
+
     # print pandas df table relating to the objects shown in image
     object_predictions = pd.DataFrame(predictions).drop('box', 1)
     object_predictions.index += 1
