34dad1e6704913e925e35934ddafb04e0201068f
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-09 11:22:17 +0100

    position labels next to image

--- scivision/vis/display.py
+++ scivision/vis/display.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import matplotlib.pyplot as plt
 from matplotlib.image import AxesImage
 import numpy as np
 import pandas as pd
@@ -34,10 +35,22 @@ def predplot(image: np.ndarray,
         bounded_image = _draw_bounding_box(pillow_image, bounding_box["score"],\
             box["xmin"], box["ymin"], box["xmax"], box["ymax"], index, num_boxes, font)
         index += 1
+    # display(bounded_image)
 
-    display(bounded_image)
-    
-    # print pandas df table relating to the objects shown in image
+    # print pandas object_predictions table relating to the objects shown in image
     object_predictions = pd.DataFrame(predictions).drop('box', 1)
     object_predictions.index += 1
-    print(object_predictions[['label', 'score']])
+    object_predictions = object_predictions[['label', 'score']]
+    
+    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax1.imshow(bounded_image)
+    ax2 = fig.add_subplot(122)
+    font_size = 14
+    bbox = [0, 0, 1, 1]
+    ax2.axis('off')
+    mpl_table = ax2.table(cellText=object_predictions.values, rowLabels=object_predictions.index, bbox=bbox, colLabels=object_predictions.columns)
+    plt.show()
