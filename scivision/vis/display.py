402f782090b0f2e39b952f9ac97f364bb9662828
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-09 10:21:27 +0100

    use font so we can set size of text labels

--- scivision/vis/display.py
+++ scivision/vis/display.py
@@ -4,13 +4,15 @@
 from matplotlib.pyplot import imshow
 from matplotlib.image import AxesImage
 import numpy as np
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
+import os.path
 
-def _draw_bounding_box(im, score, label, xmin, ymin, xmax, ymax, index, num_boxes):
+
+def _draw_bounding_box(im, score, label, xmin, ymin, xmax, ymax, index, num_boxes, font):
     """Draw a bounding boxes for object detection."""
     im_with_rectangle = ImageDraw.Draw(im)  
     im_with_rectangle.rounded_rectangle((xmin, ymin, xmax, ymax), outline = "red", width = 5, radius = 10)
-    im_with_rectangle.text((xmin+35, ymin-25), label, fill="white", stroke_fill = "red")
+    im_with_rectangle.text((xmin+35, ymin-25), label, fill="white", stroke_fill = "red", font=font)
     return im
 
 
@@ -23,12 +25,14 @@ def predplot(image: np.ndarray,
         raise NotImplementedError("Visualisation for model predictions other than object detection have not been developed yet")
     
     pillow_image = Image.fromarray(image.to_numpy(), 'RGB')
+    font_path = os.path.abspath(os.path.dirname(__file__)) + '/fonts/arial.ttf'
+    font = ImageFont.truetype(font_path, 40)
     num_boxes = len(predictions)
     index = 0
     for bounding_box in predictions:
         box = bounding_box["box"]
         bounded_image = _draw_bounding_box(pillow_image, bounding_box["score"], bounding_box["label"],\
-            box["xmin"], box["ymin"], box["xmax"], box["ymax"], index, num_boxes)
+            box["xmin"], box["ymin"], box["xmax"], box["ymax"], index, num_boxes, font)
         index += 1
 
     imshow(bounded_image)
