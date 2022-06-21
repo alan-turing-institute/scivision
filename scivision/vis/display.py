f8575ec2f3e8129d6d3bbc7d72c0d58e6246c2d9
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-21 14:47:44 +0100

    add docstring

--- scivision/vis/display.py
+++ scivision/vis/display.py
@@ -24,8 +24,27 @@ def predplot(image: np.ndarray,
              predictions: list,
              task: str = "object detection",
              label_nums: bool = False) -> AxesImage:
-    """Plot an image loaded via scivison with predictions
-    from a scivision model"""
+    """Plot an image loaded via scivison with predictions from a scivision model.
+
+    Parameters
+    ----------
+    image : np.ndarray
+        A numpy ndarray representation of an image.
+    predictions : list
+        A list of dictionaries, on for each detected object containing 'score',
+        'label' and 'box' keys.
+        'score' should be a float between 0 and 1.
+        'label' should be a string that labels the object.
+        'box' should be a dict with 'xmin', 'ymin', 'xmax' and 'ymax' keys,
+        which denote the boundaries of each box (detected object).
+    label_nums : bool = False
+        When True, bounding boxes are numbered in addition to being colored.
+        
+    Returns
+    -------
+    The input image with colored bounding boxes and an accompanying pandas
+    dataframe legend showing corresponding labels and scores for each object.
+    """
     if task != "object detection":
         raise NotImplementedError("Visualisation for model predictions other than object detection have not been developed yet")
 
