ae95ff44effacd34302fda15b9d1a0afddf2a06d
Oliver Strickson <ostrickson@turing.ac.uk>
2022-06-29 10:43:23 +0100

    Set up React json schema form with node (for datasources)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from '@rjsf/bootstrap-4';
+import schema from './datasource_schema.js'
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="app">
+          <div className="container">
+              <div className="row">
+                  <div className="col-md-6">
+                      <Form onSubmit={(input) => download("one-datasource.json", JSON.stringify(input.formData, null, 4))}
+                            uiSchema={{"ui:options": { "submitButtonOptions": { "norender": false, "submitText": "Download" } }}}
+                            schema={schema} />
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
