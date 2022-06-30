ab8de35cea07c8b12d63500746af237e1b675ab9
Oliver Strickson <ostrickson@turing.ac.uk>
2022-06-30 20:34:55 +0100

    Multi-page react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
 import Form from '@rjsf/bootstrap-4';
-import schema from './datasource_schema.js'
+import datasource_schema from './datasource_schema.js'
+import model_schema from './model_schema.js'
+
+import { Nav, NavDropdown, Navbar, Container, Alert } from "react-bootstrap";
+import SidebarMenu from 'react-bootstrap-sidebar-menu';
 
 function download(filename, text) {
   var element = document.createElement('a');
@@ -16,16 +28,56 @@ function download(filename, text) {
   document.body.removeChild(element);
 }
 
+function DataSourceForm() {
+  return (<Form onSubmit={(input) => download("one-datasource.json", JSON.stringify(input.formData, null, 4))}
+                uiSchema={{"ui:options": { "submitButtonOptions": { "norender": false, "submitText": "Download" } }}}
+                schema={datasource_schema} />);
+}
+
+function ModelForm() {
+  return (<Form onSubmit={(input) => download("one-model.json", JSON.stringify(input.formData, null, 4))}
+                uiSchema={{"ui:options": { "submitButtonOptions": { "norender": false, "submitText": "Download" } }}}
+                schema={model_schema} />);
+}
+
+function AboutText() {
+  return (<p>Add text here</p>);
+}
+
 function App() {
   return (
       <div className="app">
-          <div className="container">
-              <div className="row">
-                  <div className="col-md-6">
-                      <Form onSubmit={(input) => download("one-datasource.json", JSON.stringify(input.formData, null, 4))}
-                            uiSchema={{"ui:options": { "submitButtonOptions": { "norender": false, "submitText": "Download" } }}}
-                            schema={schema} />
-                  </div>
+          <div className="container-fluid">
+              <Navbar bg="light" expand="lg">
+                  <Navbar.Brand>
+                      <p class="h1"> <img src={logo} alt="Scivision logo" /> Scivision Catalog Utility</p>
+                  </Navbar.Brand>
+              </Navbar>
+              <div class="row px-4 mt-2">
+                  <Router>
+                      <Nav className="col-lg-2 d-block d-none sidebar">
+                          <Nav.Item>
+                              <Link to="">About</Link>
+                          </Nav.Item>                          
+                          <Nav.Item>
+                              <Link to="datasource">New datasource entry</Link>
+                          </Nav.Item>
+                          <Nav.Item>
+                              <Link to="model">New model entry</Link>
+                          </Nav.Item>
+                      </Nav>
+                      <div className="col-md-6">
+                          <Routes>
+                              <Route exact path="/" element={<AboutText/>} />
+                              <Route path="/datasource" element={
+                                             <DataSourceForm />
+                                     }/>
+                              <Route path="/model" element={
+                                             <ModelForm />
+                                     } />
+                          </Routes>
+                      </div>
+                  </Router>
               </div>
           </div>
       </div>
