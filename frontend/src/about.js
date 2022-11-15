2934024aa34ca0814318e6c55161645c745bc3e2
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-11-15 16:24:07 +0000

    use react-showdown to render markdown

--- frontend/src/about.js
+++ frontend/src/about.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import MarkdownView from 'react-showdown';
+
 // Component: Landing page
 // route: /
 export function AboutText() {
-    return (<p>Welcome to Scivision</p>);
+    const markdown = `
+  # Welcome to React Showdown :+1:
+
+  To get started, edit the markdown in \`example/src/App.tsx\`.
+
+  | Column 1 | Column 2 |
+  |----------|----------|
+  | A1       | B1       |
+  | A2       | B2       |
+  `;
+
+    return (
+      <MarkdownView
+        markdown={markdown}
+        options={{ tables: true, emoji: true }}
+      />
+    );
 }
