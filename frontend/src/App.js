509211e62edf41fca50ab6a01fdcb47eebfd7169
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-01-12 11:23:09 +0000

    use header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -409,7 +409,7 @@ function DatasourceDefinitionListFragment({data}) {
 function ProjectDefinitionListFragment({data}) {
     return (<>
   
-                <dt className="col-sm-3">[INSERT HEADER]</dt>
+                <dt className="col-sm-3">{data.header?data.header:"(none provided)"}</dt>
                 <dd className="col-sm-9">{data.description?data.description:"(none provided)"}</dd>
 
             </>);
