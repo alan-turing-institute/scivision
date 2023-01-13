202dd6c4c7510062f89c032e069d20c6cb5a5a60
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-01-13 15:58:38 +0000

    fix project schema

--- frontend/src/project_schema.js
+++ frontend/src/project_schema.js
@@ -1,25 +1,72 @@
 const schema = {
-    "title": "CatalogProjects",
+    "title": "A project catalog entry",
     "type": "object",
     "properties": {
-        "catalog_type": {
-            "title": "Catalog Type",
-            "default": "scivision project catalog",
-            "type": "string"
-        },
         "name": {
             "title": "Name",
+            "description": "Short, unique name for the project (one or two words, under 20 characters recommended)",
             "type": "string"
         },
-        "entries": {
-            "title": "Entries",
+        "header": {
+            "title": "Header",
+            "description": "Header that will display at the top of the project page",
+            "type": "string"
+        },
+        "description": {
+            "title": "Description",
+            "description": "Short description of the project (that will appear when hovering on the project thumbnail)",
+            "type": "string"
+        },
+        "page": {
+            "title": "Page",
+            "description": "Markdown formatted content for the project page",
+            "type": "string"
+        },
+        "models": {
+            "title": "Models",
+            "description": "Which models from the scivision catalog are used in the project?",
+            "default": [],
             "type": "array",
             "items": {
-                "$ref": "#/definitions/CatalogProjectEntry"
+                "type": "string"
+            }
+        },
+        "datasources": {
+            "title": "Datasources",
+            "description": "Which datasources from the scivision catalog are used in the project?",
+            "default": [],
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        },
+        "tasks": {
+            "title": "Tasks",
+            "description": "Which task (or tasks) do the CV models used in the project perform?",
+            "default": [],
+            "type": "array",
+            "items": {
+                "$ref": "#/definitions/TaskEnum"
+            }
+        },
+        "institution": {
+            "title": "Institution(s)",
+            "description": "A list of institutions that produced or are associated with the project (one per item)",
+            "default": [],
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        },
+        "tags": {
+            "title": "Tags",
+            "type": "array",
+            "items": {
+                "type": "string"
             }
         }
     },
-    "required": ["name", "entries"],
+    "required": ["name", "header", "tags"],
     "additionalProperties": false,
     "definitions": {
         "TaskEnum": {
@@ -27,77 +74,6 @@ const schema = {
             "description": "An enumeration.",
             "enum": ["classification", "object-detection", "segmentation", "thresholding", "other"],
             "type": "string"
-        },
-        "CatalogProjectEntry": {
-            "title": "A project catalog entry",
-            "type": "object",
-            "properties": {
-                "name": {
-                    "title": "Name",
-                    "description": "Short, unique name for the project (one or two words, under 20 characters recommended)",
-                    "type": "string"
-                },
-                "header": {
-                    "title": "Header",
-                    "description": "Header that will display at the top of the project page",
-                    "type": "string"
-                },
-                "description": {
-                    "title": "Description",
-                    "description": "Short description of the project (that will appear when hovering on the project thumbnail)",
-                    "type": "string"
-                },
-                "page": {
-                    "title": "Page",
-                    "description": "Markdown formatted content for the project page",
-                    "type": "string"
-                },
-                "models": {
-                    "title": "Models",
-                    "description": "Which models from the scivision catalog are used in the project?",
-                    "default": [],
-                    "type": "array",
-                    "items": {
-                        "type": "string"
-                    }
-                },
-                "datasources": {
-                    "title": "Datasources",
-                    "description": "Which datasources from the scivision catalog are used in the project?",
-                    "default": [],
-                    "type": "array",
-                    "items": {
-                        "type": "string"
-                    }
-                },
-                "tasks": {
-                    "title": "Tasks",
-                    "description": "Which task (or tasks) do the CV models used in the project perform?",
-                    "default": [],
-                    "type": "array",
-                    "items": {
-                        "$ref": "#/definitions/TaskEnum"
-                    }
-                },
-                "institution": {
-                    "title": "Institution(s)",
-                    "description": "A list of institutions that produced or are associated with the project (one per item)",
-                    "default": [],
-                    "type": "array",
-                    "items": {
-                        "type": "string"
-                    }
-                },
-                "tags": {
-                    "title": "Tags",
-                    "type": "array",
-                    "items": {
-                        "type": "string"
-                    }
-                }
-            },
-            "required": ["name", "header", "tags"],
-            "additionalProperties": false
         }
     }
 }
