75a423af1632b2a6a307a423b30e5f1be0bbfcdd
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-01-13 14:53:05 +0000

    add project form and write bullets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1319,6 +1319,39 @@ function App() {
                                       </div>
                                   </>
                               } />
+                              
+                        <Route path="/new-project" element={
+                                   <>
+                                       <ModelNav />
+                                       <h3>Add your research project to Scivision</h3>
+                                       <h4>Prerequistes</h4>
+
+                                       <p  className="text-readable-width">
+                                       <ul>
+                                       <li>You have already added the datasources used in your project to the <Link to="../datasource-grid">Scivision Data catalog</Link>. Click here to add a <Link to="../new-datasource">new datasource</Link>.</li>
+                                       <li>You have already added the computer vision models used in your project to the <Link to="../model-grid">Scivision Model catalog</Link>. Click here to add a <Link to="../new-model">new model</Link>.</li>
+                                       </ul>
+                                       </p>
+
+                                       <h4> Add your model</h4>
+
+                                       <p className="text-readable-width">
+                                          Add some details about your model below.  Submitting the form will open a pull request (from your GitHub user account) that adds details of your model to the catalog.  Further discussion is possible at that point, so it doesn't need to be complete or perfect at this stage.</p>
+
+                                          <p className="text-readable-width mt-4">
+                                          Make sure to <strong>log in with the link above</strong> before completing the form
+                                          </p>
+                                       <div className="text-readable-width mt-4">
+                                           <CatalogEntryForm
+                                               gh_logged_in={gh_logged_in}
+                                               schema={model_schema}
+                                               catalog_kind="model"
+                                               catalog_path="scivision/catalog/data/models.json"
+                                               download_filename="one-model.json"
+                                           />
+                                       </div>
+                                   </>
+                               } />
 
                         <Route path="/examples" element={
 
