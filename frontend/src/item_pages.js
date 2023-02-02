d3c6be569364fa89a27410fc19e4061a5bf944fd
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-02-02 11:21:48 +0000

    add scivision load model code again

--- frontend/src/item_pages.js
+++ frontend/src/item_pages.js
@@ -25,7 +25,18 @@ export function Model() {
     const { model_name_encoded } = useParams();
     const model_name = decodeURIComponent(model_name_encoded);
     const model = models.entries.find(model => model.name == model_name);
-
+    
+    if (model.installable){
+      var scivision_code = <>
+                            <dt className="col-sm-3"><a href="https://scivision.readthedocs.io/en/latest/api.html">Loadable with Scivision in Python</a>:</dt>
+                            <dd className="col-sm-9">
+                                <p><code>from scivision import load_pretrained_model</code></p>
+                                <p><code>load_pretrained_model({model.url})</code></p>
+                            </dd>
+                            </>;
+    } else {
+      var scivision_code = <></>;
+    }
     return (<>
                 <h3>{model.name}</h3>
                 <img src={model_thumbnails[`./${model.name}.jpg`]} />
@@ -41,6 +52,8 @@ export function Model() {
                     <dd className="col-sm-9">
                         <div><code>pip install {model.pkg_url}</code></div>
                     </dd>
+                    
+                    {scivision_code}
                   </>
                 </dl>
             </>);
