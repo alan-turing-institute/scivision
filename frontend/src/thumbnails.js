0938887129c3a5209d1aade551ac651e2086848d
Isabel Fenton <IFenton@users.noreply.github.com>
2023-02-02 11:05:56 +0000

    Refactor front end javascript (#513)
    * Move utils functions from App.js
    
    * Move configuration constants from App.js
    
    * Move code to load thumbnails from App.js
    
    * Move form to create a new catalog entry from App.js
    
    * Move github function from App.js
    
    * Move catalog navigation functions from App.js
    
    * Move functions used to create the table from App.js
    
    * Move functions used to create the grid from App.js
    
    * Move functions used to create the separate item pages from App.js
    
    * Move function used to create the home page from App.js
    
    * Tidy App.js
    
    * Tidy the imports
    
    * Only export necessary functions and constants
    
    * Inline ModelDefinitionListFragment and DatasourceDefinitionListFragment
    
    * Rename TableGridViewNav to CatalogNavBar
    
    * Remove unnecessary fragment group
    
    ---------
    
    Co-authored-by: ots22 <ots22@users.noreply.github.com>

---
+++ frontend/src/thumbnails.js
@@ -0,0 +1,32 @@
+// Load the thumbnail images
+
+// From a webpack object returned by require.context (ctxt), make a
+// dictionary from the resource name to its path
+//
+// ctxt is callable, and calling it with the name of a resource
+// returns the path to that resource.  It also has a '.keys()' method,
+// which returns all the included resources.
+//
+// Could strip the leading './' and trailing extension (and then handle
+// several file types)
+function context_to_paths(ctxt) {
+    return ctxt.keys().reduce((dict, name) => {
+        dict[name] = ctxt(name);
+        return dict;
+    }, {});
+}
+
+const model_thumbnails_ctxt = require.context(
+    './data/thumbnails/models', false, /\.jpg$/
+);
+export const model_thumbnails = context_to_paths(model_thumbnails_ctxt);
+
+const datasource_thumbnails_ctxt = require.context(
+    './data/thumbnails/datasources', false, /\.jpg$/
+);
+export const datasource_thumbnails = context_to_paths(datasource_thumbnails_ctxt);
+
+const project_thumbnails_ctxt = require.context(
+    './data/thumbnails/projects', false, /\.jpg$/
+);
+export const project_thumbnails = context_to_paths(project_thumbnails_ctxt);
