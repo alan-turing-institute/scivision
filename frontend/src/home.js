e55651c183c445753c03068094e8f2b1d8af733b
vimode <39148877+vimode@users.noreply.github.com>
2023-05-12 18:54:30 +0530

    Update homepage with bootstrap grid components

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
 import { React } from 'react';
+import { Container, Row, Col, Card } from "react-bootstrap";
+
 import datasources from './data/datasources.json';
 import models from './data/models.json';
 import projects from './data/projects.json';
@@ -33,90 +35,89 @@ export function Home() {
 
     return (
         <>
-            <div className="mb-5">
+            <Container>
                 <h4>Models</h4>
-
                 <div className="w-75 mx-auto m-3">
-
                     <p className="small">Pre-trained computer vision models that can be loaded and run with the <Link to="scivisionpy">Scivision Python library.</Link></p>
 
-                    <div className="card-deck">
+                    <Row xs={1} sm={3} className="justify-content-md-center">
                         {
                             models_sample.map((model) => (
-                                <div className="card" key = {model.name}>
-                                    {
-                                        makeThumbnail({
-                                            getThumbnail: (data) => model_thumbnails[`./${data.name}.jpg`],
-                                            getLink: (data) => "/model/" + encodeURIComponent(data.name),
-                                            doPopover: true,
-                                        })(model)
-                                    }
-                                </div>
+                                <Col>
+                                        <Card className="mb-3 mb-sm-0" key = {model.name}>
+                                            {
+                                                makeThumbnail({
+                                                    getThumbnail: (data) => model_thumbnails[`./${data.name}.jpg`],
+                                                    getLink: (data) => "/model/" + encodeURIComponent(data.name),
+                                                    doPopover: true,
+                                                })(model)
+                                            }
+                                        </Card>
+                                </Col>
                             ))
                         }
-                    </div>
+                    </Row>
                     <p className="p-1 pl-2 small bg-highlight">
                         Discover more models in the complete <Link to="model-grid"><strong>model catalog</strong></Link>
                     </p>
                 </div>
-            </div>
+            </Container>
 
 
-            <div className="mb-5">
+            <Container>
                 <h4>Data</h4>
-
                 <div className="w-75 mx-auto m-3">
                     <p className="small">Curated image datasets from diverse scientific domains, suitable for a variety of computer vision tasks and loadable as array data via the numerical Python stack.</p>
-                    <div className="card-deck">
+                    <Row xs={1} sm={3} className="justify-content-md-center">
                         {
                             datasources_sample.map((ds) => (
-                                <div className="card" key={ds.name}>
-                                    {
-                                        makeThumbnail({
-                                            getThumbnail: (data) => datasource_thumbnails[`./${data.name}.jpg`],
-                                            getLink: (data) => "/datasource/" + encodeURIComponent(data.name),
-                                            doPopover: true,
-                                        })(ds)
-                                    }
-                                </div>
+                                  <Col>
+                                          <Card  className="mb-3 mb-sm-0" key={ds.name}>
+                                            {
+                                                makeThumbnail({
+                                                    getThumbnail: (data) => datasource_thumbnails[`./${data.name}.jpg`],
+                                                    getLink: (data) => "/datasource/" + encodeURIComponent(data.name),
+                                                    doPopover: true,
+                                                })(ds)
+                                            }
+                                        </Card>
+                                  </Col>
                             ))
                         }
-                    </div>
-                    <div className="bg-highlight">
-                        <p className="p-1 pl-2 small">
+                    </Row>
+                        <p className="p-1 pl-2 small bg-highlight">
                             Explore the full <Link to="datasource-grid"><strong>datasource catalog</strong></Link>
                         </p>
-                    </div>
                 </div>
-            </div>
+            </Container>
 
-            <div className="mb-5">
+            <Container>
                 <h4>Projects</h4>
 
                 <div className="w-75 mx-auto m-3">
                     <p className="small">Research projects that have contributed scientific image data and computer vision models to the Scivision catalog.</p>
-                    <div className="card-deck">
+                    <Row xs={1} sm={3} className="justify-content-md-center">
                         {
                             projects_sample.map((proj) => (
-                                <div className="card" key={proj.name}>
-                                    {
-                                        makeThumbnail({
-                                            getThumbnail: (project) => project_thumbnails[`./${project.name}.jpg`],
-                                            getLink: (project) => "/project/" + encodeURIComponent(project.name),
-                                            doPopover: true,
-                                        })(proj)
-                                    }
-                                </div>
+                                <Col>
+                                        <Card  className="mb-3 mb-sm-0" key={proj.name}>
+                                            {
+                                                makeThumbnail({
+                                                    getThumbnail: (project) => project_thumbnails[`./${project.name}.jpg`],
+                                                    getLink: (project) => "/project/" + encodeURIComponent(project.name),
+                                                    doPopover: true,
+                                                })(proj)
+                                            }
+                                        </Card>
+                                </Col>
                             ))
                         }
-                    </div>
-                    <div className="bg-highlight">
-                        <p className="p-1 pl-2 small">
+                    </Row>
+                        <p className="p-1 pl-2 small bg-highlight">
                             Explore the full <Link to="project-grid"><strong>project catalog</strong></Link>
                         </p>
-                    </div>
                 </div>
-            </div>
+            </Container>
 
         </>
     );
