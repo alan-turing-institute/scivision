fb45655c12423cc46445bc2b716012e905500cb4
ots22 <ots22@users.noreply.github.com>
2023-07-10 22:39:39 +0100

    Use explicit UTF-8 encoding for files read in setup.py (#589)

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import find_packages, setup
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 requirements = []
-with open("requirements.txt") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     for line in f:
         stripped = line.split("#")[0].strip()
         if len(stripped) > 0:
