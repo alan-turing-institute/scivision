7faa923a8e197a1cd3b7e192903a617633105f3b
Oliver Strickson <ostrickson@turing.ac.uk>
2023-08-30 12:15:19 +0100

    Image upload (first go)

--- frontend/src/DatasourceNew.js
+++ frontend/src/DatasourceNew.js
@@ -1,8 +1,80 @@
+import { useState, useRef } from "react";
 import { Row, Col } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Dropzone from "react-dropzone";
+import AvatarEditor from "react-avatar-editor";
 import CatalogEntryForm from "./CatalogEntryForm.js";
 import DatasourceNav from "./DatasourceNav.js";
 
 import datasource_schema from "./catalog/datasource_schema.js";
+
+function ImageUpload() {
+    const editor = useRef(null);
+    const [haveImage, setHaveImage] = useState(false);
+    const [image, setImage] = useState("");
+
+    function handleUpdateImage(e) {
+        setImage(URL.createObjectURL(e.target.files[0]));
+        setHaveImage(true);
+    }
+
+    return (
+        <div>
+            { haveImage ?
+              (<AvatarEditor
+                   ref={editor}
+                   image={image}
+                   width={256}
+                   height={256}
+                   scale={1.0}
+                   border={20.0}
+               />) :
+              (<div
+                   style={{
+                       "width": "296px",
+                       "height": "296px",
+                       "background-color": "#eeeeee",
+                       "border-style": "inset",
+                       "border-width": "2px",
+                       "border-color": "lightGray",
+                       "display": "flex",
+                       "align-items": "center",
+                       "justify-content": "center",
+                       "padding": "20px",
+                       "font-size": "small"
+                   }}
+                   className="m-1"
+               >
+                   Select a thumbnail image to upload below
+               </div>)
+            }
+            <Form.Control type="range" disabled={!haveImage} width=""/>
+            <Form.Control type="file" accept="image/*" onChange={handleUpdateImage} className="my-3"/>
+            <Button
+                variant="outline-secondary"
+                size="sm"
+                onClick={() => {
+                    setImage(null);
+                    setHaveImage(false);
+                }}>
+                Clear selected image
+            </Button>
+            <button onClick={() => {
+                        if (editor) {
+                            // This returns a HTMLCanvasElement, it can be made into a data URL or a blob,
+                            // drawn on another canvas, or added to the DOM.
+                            // window.canvas = editor.current.getImage();
+
+                            // If you want the image resized to the canvas size (also a HTMLCanvasElement)
+                            window.canvasScaled = editor.current.getImageScaledToCanvas().toDataURL("image/jpeg");
+                            window.haveImage = haveImage;
+                        }
+                    }}>Save</button>
+
+        </div>
+    );
+}
 
 export default function DatasourceNew({ gh_logged_in }) {
     return (
@@ -30,6 +102,11 @@ export default function DatasourceNew({ gh_logged_in }) {
 
                     <p>Make sure to <strong>log in with the link above</strong> before completing the form</p>
                 </Col>
+
+                <Col md={{ span:8, offset: 2}}>
+                    <ImageUpload />
+                </Col>
+
                 <Col md={{ span:8, offset: 2}}>
                     <CatalogEntryForm
                         gh_logged_in={gh_logged_in}
