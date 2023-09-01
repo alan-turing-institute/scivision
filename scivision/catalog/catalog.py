707a3f4ff7772fcf6594dfb6846bfa72fae7747d
Oliver Strickson <ostrickson@turing.ac.uk>
2023-09-01 14:56:29 +0100

    Add example

--- scivision/catalog/catalog.py
+++ scivision/catalog/catalog.py
@@ -140,6 +140,7 @@ class CatalogDatasourceEntry(
         
 - Good example :white_check_mark:: `https://example.com/path/to/datasource.yml` (location of the scivision yml file)
 - Okay example :heavy_check_mark:: `https://example.com/dataset/download/data.zip` (data download URL)
+- Bad example :x:: `example.com/path/to/datasource.yml` (missing URL scheme, like 'https://')
 """,
     )
     tasks: FrozenSet[TaskEnum] = Field(
