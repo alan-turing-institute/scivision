5db411609e71f34392624f47d676f2d2dba0efd9
Samuel Tonks <60216815+Tonks684@users.noreply.github.com>
2024-06-19 12:55:39 +0100

    Check Models Action and Front End showing Action findings (#687)
    * Check Models Action
    
    * test for nod20 actions
    
    * toml updates
    
    * entry point
    
    * entry point
    
    * entry point
    
    * allow install true
    
    * debuging check model outputs when unable to install package via pip
    
    * debuging check model outputs when unable to install package via pip
    
    * debuging check model outputs when unable to install package via pip
    
    * subprocess.check_call shell=True
    
    * model catalog checks
    
    * model catalog checks
    
    * model catalog checks
    
    * model catalog checks requests
    
    * model catalog action
    
    * model catalog action
    
    * check_models
    
    * url updates
    
    * url update VEdge
    
    * print config
    
    * Update path to use os.path.join
    
    * model load successfully check
    
    * flake8 updates
    
    * flak8 updates
    
    * flake8 updates
    
    * workflow schedule updated
    
    * updates to function description and return report
    
    * check models formatting
    
    * save action report as csv
    
    * temporary change of run action on push so to check results
    
    * check model report
    
    * model report
    
    * json fix
    
    * stardist fix
    
    * stardist fix
    
    * json serializable
    
    * falke8
    
    * flake8
    
    * update website with check models
    
    * test
    
    * json changes
    
    * json changes
    
    * json changes
    
    * bug
    
    * bug
    
    * checkreport
    
    * switch back to at
    
    * removal of artefact saving in yml and updates to checkreport in Model Table
    
    * turing to fork
    
    * table not loading debugging
    
    * removal of store artifact component of action
    
    * model table now populates test outcomes, paths sets back to alan turing institute, store artifact is removed and tested to make sure still runs successfully
    
    ---------
    
    Co-authored-by: Isabel Fenton <IFenton@users.noreply.github.com>

--- frontend/src/ModelTable.jsx
+++ frontend/src/ModelTable.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import useScript from "react-use-scripts";
 import DataTable from "react-data-table-component";
 
 import ModelNav from "./ModelNav.jsx";
@@ -11,10 +13,10 @@ import { TaskBadge } from "./badges.jsx";
 import models from "./catalog/data/models.json";
 
 // Component: Fragment containing definition items for the expanded
-// view of the model table, and the model page
+// view of the Model table and the page for one Model
 //
-// * data - one model
-function ModelDefinitionList({ data }) {
+// * data - one Model
+function modelDefinitionList({ data }) {
   return (
     <dl className="row">
       <dt className="col-sm-3">Description</dt>
@@ -22,27 +24,51 @@ function ModelDefinitionList({ data }) {
         {data.description ? data.description : "(none provided)"}
       </dd>
 
-      <dt className="col-sm-3">Homepage</dt>
+      <dt className="col-sm-3">Location</dt>
       <dd className="col-sm-9">
         <a href={data.url}>{data.url}</a>
-      </dd>
-
-      <dt className="col-sm-3">Install with pip</dt>
-      <dd className="col-sm-9">
-        <div>
-          <code>pip install {data.pkg_url}</code>
-        </div>
       </dd>
     </dl>
   );
 }
 
+// Component: Models, table view
+// route: /Models
 function ModelTableContents() {
+  const [modelChecksReport, setmodelChecksReport] = useState(null);
+
+  function modelCheckResult(name) {
+    if (modelChecksReport !== null) {
+      const report = modelChecksReport.report[name];
+      if (report !== undefined) {
+        return report.check_result;
+      } else {
+        return "Unknown";
+      }
+    } else {
+      return "Unknown";
+    }
+  }
+
+  function modelCheckTime() {
+    if (modelChecksReport) {
+      var time = new Date(modelChecksReport.time);
+      return time.toUTCString();
+    } else {
+      return "(never)";
+    }
+  }
+
+  function modelValidationTimeString() {
+    return `last run ${modelCheckTime()}`;
+  }
+
   const columns = [
     {
       name: "Thumbnail",
       width: "150px",
-      selector: (row) => model_thumbnails[`./${row.name}.jpg`] === undefined,
+      selector: (row) =>
+        model_thumbnails[`./${row.name}.jpg`] === undefined,
       sortable: true,
       cell: (row, index, column, id) => {
         const thumb = model_thumbnails[`./${row.name}.jpg`];
@@ -50,18 +76,66 @@ function ModelTableContents() {
       },
     },
     {
+      selector: (row) => row.name,
       name: "Name",
       sortable: true,
       grow: 0.5,
-      selector: (row) => row.name,
     },
     {
-      name: "Tasks",
       selector: (row) => row.tasks,
+      name: "Tasks",
       cell: (row, index, column, id) =>
         row.tasks.map((t) => <TaskBadge key={t} taskName={t} />),
     },
+    {
+      selector: (row) => {
+        const result = modelCheckResult(row.name);
+        if (result === "Pass") {
+          return (
+            <img
+              src="https://img.shields.io/badge/scivision_metadata-pass-green"
+              title="The metadata for this model was successfully loaded by scivision, from the location in the catalog"
+            />
+          );
+        } else if (result === "Fail") {
+          return (
+            <img
+              src="https://img.shields.io/badge/scivision_metadata-fail-red"
+              title="Scivision metadata (yaml) file for this model failed to load or was missing at the indicated location"
+            />
+          );
+        } else {
+          return (
+            <img
+              src="https://img.shields.io/badge/scivision_metadata-unknown-lightgray"
+              title="Could not access the result for this validation check"
+            />
+          );
+        }
+      },
+      name: (
+        <span
+          className="tooltip-available"
+          title={modelValidationTimeString()}
+        >
+          Validation checks
+        </span>
+      ),
+      grow: 0.5,
+    },
   ];
+
+  const check_models_script_url =
+  "https://github.com/alan-turing-institute/scivision/releases/download/model-checks-report-latest-release/check_models.js"
+
+  useScript({
+    src: check_models_script_url,
+    onReady: () => setmodelChecksReport(window.global_CheckModelReport),
+    onError: () =>
+      console.log(
+        `Could not load latest model checks from ${check_models_script_url}`,
+      ),
+  });
 
   return (
     <DataTable
@@ -69,7 +143,7 @@ function ModelTableContents() {
       data={models.entries}
       title=""
       expandableRowsComponent={(props) => (
-        <TableCardDropdown element={<ModelDefinitionList {...props} />} />
+        <TableCardDropdown element={<modelDefinitionList {...props} />} />
       )}
       expandableRows
       expandableRowsHideExpander
@@ -79,7 +153,7 @@ function ModelTableContents() {
 }
 
 // Component: Models, table view
-// route: /models
+// route: /Models
 export default function ModelTable() {
   return (
     <>
