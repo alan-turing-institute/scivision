29d3de77823c07af6f8161d10f386bdd79f81a02
Isabel Fenton <IFenton@users.noreply.github.com>
2025-10-31 15:41:28 +0000

    Testing file paths (#724)
    * Testing file paths
    
    * Changing vite base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), nodePolyfills()],
-    base: '/',
+    base: '/scivision/',
     resolve: {
         alias: {
             'node-fetch': 'isomorphic-fetch',
